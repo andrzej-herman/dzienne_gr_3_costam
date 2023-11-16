10885885ea227b52daee0772452d1a2db5452922
Andrzej Herman <andrzejek.herman@gmail.com>
2023-11-16 12:49:29 +0100

    Add project

---
+++ app/movie/page.tsx
@@ -0,0 +1,7 @@
+export default function Movie() {
+    return (
+        <div className="h-full flex items-center justify-center">
+            <p className="text-7xl text-pink-800 tracking-tight">Movie</p>
+        </div>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+      <div className="h-full flex items-center justify-center">
+        <p className="text-7xl text-pink-800 tracking-tight">Społeczna Akademia Nauk</p>
+      </div>
+  )
+}
+
+
+
+
+
+
